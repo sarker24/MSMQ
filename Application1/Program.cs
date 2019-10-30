57d60e8b757c53b99a8746de979a2536f1e29fb8
Murad <sarker24@gmail.com>
2019-10-30 19:38:40 +0100

    First commit

---
+++ Application1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Messaging;
+
+namespace Application1
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your message to be sent");
+            Console.WriteLine("High prioroty message mark as 'HP:' ");
+            string MessageSend = Console.ReadLine();
+
+            MessageQueue myQueue;
+            if(MessageQueue.Exists(@".\Private$\MyQueue"))
+            {
+                myQueue = new MessageQueue(@".\Private$\MyQueue");
+            }
+            else
+            {
+                myQueue = MessageQueue.Create(@".\Private$\MyQueue");
+            }
+
+            Message MyMessage = new System.Messaging.Message();
+
+            MyMessage.Formatter = new BinaryMessageFormatter();
+            MyMessage.Body = MessageSend;
+            MyMessage.Label = "FromAppication1";
+           
+
+            if (MessageSend.Contains("Hi:"))
+            {
+                MyMessage.Priority = MessagePriority.Highest;
+            }
+            else
+            {
+                MyMessage.Priority = MessagePriority.Normal;
+            }
+            myQueue.Send(MyMessage);
+            Console.WriteLine("Thangs for sending message");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Application2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Latest message");
+            MessageQueue myQueue;
+            myQueue = new MessageQueue(@".\Private$\MyQueue");
+
+            Message MyMessage = myQueue.Receive();
+            MyMessage.Formatter = new BinaryMessageFormatter();
+
+            Console.WriteLine(MyMessage.Body.ToString());
+            Console.ReadKey();
+
+        }
+    }
+}
